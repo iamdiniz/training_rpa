61cdcd96b8dc5fee6889cabd6175afde40eefe59
iamdiniz <guilhermeplays03@gmail.com>
2025-03-27 12:27:50 -0300

    add new comments

--- quest_01.py
+++ quest_01.py
@@ -1,5 +1,7 @@
 numeros = [1, 2, 4, 5]
 
+# - 1 se refere ao ultimo item da lista.
+# o + 1 é para garantir que o último item esteja incluso na comparação.
 for numero in range(numeros[0], numeros[-1] + 1):
     if numero not in numeros:
         print(numero)
--- quest_02.py
+++ quest_02.py
@@ -1,5 +1,6 @@
 numeros = [3, 1, 2, 4, 7, 6, 9, 8]
 
+# List comprehension | [expressão for item in iterável if condição]
 pares = [n for n in numeros if n % 2 == 0]
 impares = [n for n in numeros if n % 2 != 0]
 
